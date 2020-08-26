574c7fe30216f7d5f48afde3849c137e15d9dae1
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-08-25 23:11:40 -0300

    Adds only eQUIC main to equic_user.c

--- src/equic_user.c
+++ src/equic_user.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
@@ -8,48 +7,21 @@
 #include <libgen.h>
 #include <net/if.h>
 
+/* Linux OS dependent */
 #include <sys/resource.h>
 #include <linux/bpf.h>
 #include <linux/if_link.h>
 
+
+/* libbpf included headers */
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
 
-static int ifindex;
-static __u32 prog_id;
-
-static void int_exit(int sig)
-{
-	__u32 curr_prog_id = 0;
-
-	if (bpf_get_link_xdp_id(ifindex, &curr_prog_id, XDP_FLAGS_UPDATE_IF_NOEXIST)) {
-		printf("bpf_get_link_xdp_id failed\n");
-		exit(1);
-	}
-	if (prog_id == curr_prog_id)
-		bpf_set_link_xdp_fd(ifindex, -1, XDP_FLAGS_UPDATE_IF_NOEXIST);
-	else if (!curr_prog_id)
-		printf("couldn't find a prog id on a given interface\n");
-	else
-		printf("program on interface changed, not removing\n");
-	exit(0);
-}
-
-/* simple per-protocol drop counter
- */
-static void poll_stats(int map_fd, int interval)
-{
-	while (1) {
-		sleep(interval);
-		printf("Reading counters map\n");
+#include "equic_user.h"
 
 
-
-
-	}
-}
-
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
 	struct bpf_prog_load_attr prog_load_attr = {
 		.prog_type	= BPF_PROG_TYPE_XDP,
@@ -65,8 +37,8 @@ int main(int argc, char **argv)
 	int err;
 
 
-	ifindex = if_nametoindex("eth0");
-	if (!ifindex) {
+	EQUIC_IFINDEX = if_nametoindex("eth0");
+	if (!EQUIC_IFINDEX) {
 		perror("if_nametoindex");
 		return 1;
 	}
@@ -88,10 +60,10 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	signal(SIGINT, int_exit);
-	signal(SIGTERM, int_exit);
+	signal(SIGINT, sigint_callback);
+	signal(SIGTERM, sigterm_callback);
 
-	if (bpf_set_link_xdp_fd(ifindex, prog_fd, XDP_FLAGS_UPDATE_IF_NOEXIST) < 0) {
+	if (bpf_set_link_xdp_fd(EQUIC_IFINDEX, prog_fd, EQUIC_XDP_FLAGS) < 0) {
 		printf("link set xdp fd failed\n");
 		return 1;
 	}
@@ -101,7 +73,7 @@ int main(int argc, char **argv)
 		printf("can't get prog info - %s\n", strerror(errno));
 		return err;
 	}
-	prog_id = info.id;
+	EQUIC_PROG_ID = info.id;
 
 
 	poll_stats(map_fd, 2);
