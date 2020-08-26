e3bcb155d7fbebdbdbf85fdeba6d53673ccdecde
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-08-26 18:19:03 -0300

    Removes unecessary imports

--- src/equic_user.h
+++ src/equic_user.h
@@ -2,36 +2,6 @@
 #define EQUIC_H
 
 
-#include <errno.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <libgen.h>
-#include <net/if.h>
-
-
-/* Linux OS dependent */
-#include <sys/resource.h>
-#include <linux/bpf.h>
-#include <linux/if_link.h>
-
-
-/* libbpf included headers */
-#include <bpf/bpf.h>
-#include <bpf/libbpf.h>
-
-
-/* Network interface index */
-static int EQUIC_IFINDEX;
-
-/* eBPF program ID */
-static __u32 EQUIC_PROG_ID;
-
-/* XDP Flags used for eQUIC program */
-static unsigned int EQUIC_XDP_FLAGS = XDP_FLAGS_UPDATE_IF_NOEXIST;
-
 /* Struct used to store eQUIC public variables */
 struct equic_meta
 {
@@ -58,17 +28,17 @@ typedef struct equic_meta equic_t;
  * Public functions of eQUIC module
  *
  */
-static void equic_sigint_callback (int signal);
+void equic_sigint_callback (int signal);
 
-static void equic_sigterm_callback (int signal);
+void equic_sigterm_callback (int signal);
 
-static void equic_get_interface (equic_t *equic, char if_name[]);
+void equic_get_interface (equic_t *equic, char if_name[]);
 
-static void equic_read_object (equic_t *equic, char bpf_prog_path[]);
+void equic_read_object (equic_t *equic, char bpf_prog_path[]);
 
-static void equic_load (equic_t *equic);
+void equic_load (equic_t *equic);
 
-static void equic_sync_counters (int map_fd, int interval);
+void equic_sync_counters (int map_fd, int interval);
 
 
 #endif /* EQUIC_H */
