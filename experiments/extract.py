859a51f4e83fa77359175b0a98f7d0394373f86a
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-10-28 11:13:24 -0300

    Creates method to extract requests per second from logs

--- experiments/extract.py
+++ experiments/extract.py
@@ -22,6 +22,23 @@ def extract_request_time(data, base_name):
         ofile.write("{0}\n".format(value))
 
 
+def extract_requests_per_second(data, base_name):
+
+    splitted = data.split("=")
+
+    key = splitted[0]
+    value = splitted[1]
+
+    if key != "RequestsPerSecond":
+        return
+
+    if int(value) > 0:
+        ofilename = OUT_DIR + "reqs_per_second-" + base_name + ".dat"
+
+        with open(ofilename, "a") as ofile:
+            ofile.write("{0}\n".format(value))
+
+
 def extract(log):
 
     test_type = log.split("-")[-2]  # for example: baseline
@@ -35,6 +52,7 @@ def extract(log):
 
             data = line.split()
             extract_request_time(data[1], base_name)
+            extract_requests_per_second(data[1], base_name)
 
     print("[Extracted] Type={0}, Size={1}, To={2}".format(
         test_type, test_size, OUT_DIR)
