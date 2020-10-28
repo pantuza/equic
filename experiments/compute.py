3ecc939ff09986cca770242c90a33442b4ef1103
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-10-28 11:12:52 -0300

    Updates code to use harmonic mean instead of regular mean

--- experiments/compute.py
+++ experiments/compute.py
@@ -1,7 +1,7 @@
 from sys import argv
 from sys import exit
 
-from statistics import mean
+from statistics import harmonic_mean
 
 
 OUT_DIR = "stats/"
@@ -16,16 +16,33 @@ def compute_avg_duration(dat):
         for duration in ifile:
             durations.append(float(duration))
 
-    return mean(durations)
+    return harmonic_mean(durations)
+
+def compute_avg_reqs_per_second(dat):
+
+    reqs = []
+
+    with open(dat) as ifile:
+
+        for nreqs in ifile:
+            reqs.append(int(nreqs))
+
+    return harmonic_mean(reqs)
 
 def compute(dat, meta):
 
     ofilename = OUT_DIR + meta["metric"] + "-" + meta["type"] + ".csv"
 
-    avg_duration = compute_avg_duration(dat)
+    avg_value = 0.0
+
+    if meta["metric"] == "duration":
+        avg_value = compute_avg_duration(dat)
+
+    elif meta["metric"] == "reqs_per_second":
+        avg_value = compute_avg_reqs_per_second(dat)
 
     with open(ofilename, "a") as ofile:
-        ofile.write("{0}, {1}\n".format(meta["size"], avg_duration))
+        ofile.write("{0}, {1}\n".format(meta["size"], avg_value))
 
     print("[Computed] Type={0}, Size={1}, To={2}".format(
         meta["type"], meta["size"], OUT_DIR)
