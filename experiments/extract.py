1a4324b8ae4140afe940452ca0a60660996be36e
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-11-06 18:19:27 -0300

    Creates method to extract block duration times from logs

--- experiments/extract.py
+++ experiments/extract.py
@@ -11,10 +11,11 @@ def extract_request_time(data, base_name):
     splitted = data.split("=")
 
     key = splitted[0]
-    value = splitted[1][:-1]
 
     if key != "RequestDuration":
         return
+
+    value = splitted[1][:-1]
 
     ofilename = OUT_DIR + "duration-" + base_name + ".dat"
 
@@ -27,16 +28,35 @@ def extract_requests_per_second(data, base_name):
     splitted = data.split("=")
 
     key = splitted[0]
-    value = splitted[1]
 
     if key != "RequestsPerSecond":
         return
+
+    value = splitted[1]
 
     if int(value) > 0:
         ofilename = OUT_DIR + "reqs_per_second-" + base_name + ".dat"
 
         with open(ofilename, "a") as ofile:
             ofile.write("{0}\n".format(value))
+
+
+def extract_cpu_time(data, base_name):
+
+    splitted = data.split("=")
+
+    key = splitted[0]
+
+    if key != "BlockDuration":
+        return
+
+    value = splitted[1][:-1]
+
+    if float(value) > 0.0:
+        ofilename = OUT_DIR + "block_duration-" + base_name + ".dat"
+
+        with open(ofilename, "a") as ofile:
+            ofile.write("{0}\n".format(float(value)))
 
 
 def extract(log):
@@ -53,6 +73,9 @@ def extract(log):
             data = line.split()
             extract_request_time(data[1], base_name)
             extract_requests_per_second(data[1], base_name)
+            extract_cpu_time(data[1], base_name)  # Extracts for Userspace
+            if len(data) > 6:
+                extract_cpu_time(data[6], base_name)  # Extracts for Kernelspace
 
     print("[Extracted] Type={0}, Size={1}, To={2}".format(
         test_type, test_size, OUT_DIR)
