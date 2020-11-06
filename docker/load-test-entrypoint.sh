536e18bca4b085067d8f4bf859a9c64519231965
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-11-06 18:17:27 -0300

    Adds a wait time before starting the full load test

--- docker/load-test-entrypoint.sh
+++ docker/load-test-entrypoint.sh
@@ -5,19 +5,44 @@ N_REQS=1000;
 
 
 # Default size of the payload expected in the response
-if [ ! -z "$1" ]; then
+if [ -z "${REQ_SIZE}" ]; then
     REQ_SIZE="64k";
 fi;
+echo "Load test will ask for responses with with ${REQ_SIZE}"
 
 
 # Current container name
 HOSTNAME=$(hostname)
+
+# Time to wait before triggering parallel load
+SLEEP_TIME=3
+
+#
+# Checks for parallel execution. Clients that triggers parallel
+# requests are the ones that reach the quota limit. In other words,
+# they are the attacker containers
+#
+if [[ ${PARALLEL} -eq 1 ]]; then
+
+    echo "Going to run in parallel in ${SLEEP_TIME} seconds.."
+
+    # Attacker containers waits 30 seconds before starting
+    # sending load to the server
+    sleep ${SLEEP_TIME};
+fi;
 
 #
 # Load test execution loop
 #
 for i in $(seq ${N_REQS}); do
 
-    echo "Request $i from $HOSTNAME with response size $REQ_SIZE"
-    make http_client REQ_SIZE=${REQ_SIZE} > /dev/null;
+
+    if [[ ${PARALLEL} -eq 1 ]]; then
+        echo "Parallel request $i from $HOSTNAME with response size $REQ_SIZE"
+        make http_client REQ_SIZE=${REQ_SIZE} > /dev/null &
+
+    else
+        echo "Request $i from $HOSTNAME with response size $REQ_SIZE"
+        make http_client REQ_SIZE=${REQ_SIZE} > /dev/null
+    fi;
 done;
